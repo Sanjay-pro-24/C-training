c68c6ed77ee8e13c27303942cf7e25f5246f2b42
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-09 16:02:54 +0530

    Add files via upload

---
+++ print the quotient of two intergers using division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,quotient,remainder;
+    scanf("%d%d",&a,&b);
+    quotient=a/b;
+    remainder=a%b;
+    printf("quotient=");
+    printf("%d ",quotient);
+    printf("remainder=");
+    printf("%d",remainder);
+    return 0;
+    
+}
