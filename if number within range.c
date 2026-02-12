ae645f9a21e636639158c0fc62be9ddbb7fe6425
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-12 11:42:30 +0530

    Add files via upload

---
+++ if number within range.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>=10&&num<=20){
+        printf("Number within range");
+    }
+    else{
+        printf("Not Number within range");
+    }
+    return 0;
+}
+    
