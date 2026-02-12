5f0eb674392e193c5604c40d72950297ef6835af
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-12 10:59:51 +0530

    Add files via upload

---
+++ upper and lowercase.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch>=65&&ch<=90){
+        printf("uppercase");
+    }
+    else if(ch>=97&&ch<=122){
+        printf("lowercase");
+    }
+    else{
+        printf("Not case");
+    }
+    return 0;
+}
