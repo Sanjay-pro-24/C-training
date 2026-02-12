8e88ab4a1f383cbfd574f81ae725c07876f8d56b
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-12 10:22:05 +0530

    Add files via upload

---
+++ Grade.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int mark;
+    printf("Enter a mark: ");
+    scanf("%d",&mark);
+    if(mark>=90){
+        printf("Grade A");
+    }
+    else if(mark>=75&&mark<=90){
+        printf("Grade B");
+    }
+    else{
+        printf("Grade C");
+    }
+    return 0;
+}
