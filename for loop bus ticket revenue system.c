3ab516a6320a7887bcdf4506522902e366a0744d
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-17 15:48:47 +0530

    Add files via upload

---
+++ for loop bus ticket revenue system.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int fare;
+    int total = 0;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+        scanf("%d", &fare);
+        total = total + fare;
+    }
+    printf("Total Collection: ₹%d", total);
+    return 0;
+}
