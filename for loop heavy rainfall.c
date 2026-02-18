b4a45aa9014553336eee00ad361c43f68df3ef06
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-18 11:02:03 +0530

    Add files via upload

---
+++ for loop heavy rainfall.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int rain;
+    int total = 0;
+    int heavyDays = 0;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+        scanf("%d", &rain);
+        total = total + rain;
+        if(rain > 50) {
+            heavyDays++;
+        }
+    }
+    printf("Total Rainfall: %d\n", total);
+    printf("Heavy Rain Days: %d", heavyDays);
+    return 0;
+}
