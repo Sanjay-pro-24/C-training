6e0a9a918434d419b8dfdfe436fe26f86156adfc
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-20 11:38:44 +0530

    Add files via upload

---
+++ while loop noise.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int noise;
+    int i=0,violations=0,currentStreak,longestStreak=0;
+    while(i < N) {
+        scanf("%d", &noise);
+        if(noise>70) {
+            violations++;
+            currentStreak++;
+            if(currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+            }
+        } else {
+            currentStreak=0;
+        }
+        i++;
+    }
+    printf("Noise Violations: %d\n", violations);
+    printf("Longest Violation Streak: %d", longestStreak);
+    return 0;
+}
