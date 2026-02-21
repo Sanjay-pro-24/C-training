4313fd16c55c59dd8813970460d496d9d5fa7a96
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-02-21 09:46:40 +0530

    Add files via upload

---
+++ while loop online order cancel.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int N,status,i=0,successful=0,cancelled=0;
+    scanf("%d",&N);
+    while(i < N)
+    {
+        scanf("%d",&status);
+        if(status == 1)
+        {
+            successful = successful + 1;
+        }
+        else if(status == 0)
+        {
+            cancelled = cancelled + 1;
+        }
+        i = i + 1;
+    }
+    printf("Successful: %d\n",successful);
+    printf("Cancelled: %d\n",cancelled);
+    if(cancelled > successful)
+    {
+        printf("Status: Risk");
+    }
+    else
+    {
+        printf("Status: Safe");
+    }
+    return 0;
+}
