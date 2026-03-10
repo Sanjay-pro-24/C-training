464c2593724490d8022309d8402b6838e340d12b
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-03-10 21:10:06 +0530

    Add files via upload

---
+++ 2d column.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int R, C;
+    scanf("%d %d",&R,&C);
+    int a[R][C];
+    for(int i=0;i<R;i++)
+    {
+        for(int j=0;j<C;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int maxSum=-1000000000;
+    int index=-1;
+    for(int j=0;j<C;j++)
+    {
+        int sum=0;
+        for(int i=0;i<R;i++)
+        {
+            sum+=a[i][j];
+        }
+        if(sum>maxSum)
+        {
+            maxSum=sum;
+            index=j;
+        }
+    }
+
+    printf("%d",index);
+    return 0;
+}
