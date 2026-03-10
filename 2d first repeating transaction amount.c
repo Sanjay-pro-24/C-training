5cb6553b21d333560133e31ee2016417aa6753ad
Sanjay-pro-24 <kathirvelsanjay2007@gmail.com>
2026-03-10 13:40:41 +0530

    Add files via upload

---
+++ 2d first repeating transaction amount.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    int R,C;
+    scanf("%d %d",&R,&C);
+    int a[R][C];
+    for(int i=0;i<R;i++)
+    {
+        for(int j=0;j<C;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int found=0;
+    for(int i=0;i<R;i++)
+    {
+        for(int j=0;j<C;j++)
+        {
+            for(int k=i;k<R;k++)
+            {
+                for(int l=0;l<C;l++)
+                {
+                    if(k==i && l<=j)
+                    {
+                        continue;
+                    }
+                    if(a[i][j]==a[k][l])
+                    {
+                        printf("%d",a[i][j]);
+                        found=1;
+                        break;
+                    }
+                }
+                if(found==1)
+                {
+                    break;
+                }
+            }
+            if(found==1)
+            {
+                break;
+            }
+        }
+        if(found==1)
+        {
+            break;
+        }
+    }
+    return 0;
+}
